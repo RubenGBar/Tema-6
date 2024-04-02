b129eff11ed45a11de4210cc336239c65667dbd0
RubenGBar <ruben.garcia@iesnervion.es>
2024-04-02 14:35:52 +0200

    ejercicio 1 terminado sin comentar

--- src/boletin1/ejercicio1/Hora.java
+++ src/boletin1/ejercicio1/Hora.java
@@ -2,4 +2,72 @@ package boletin1.ejercicio1;
 
 public class Hora {
 
+	protected int hora = 0;
+	protected int minuto = 0;
+
+	public Hora(int hora, int minuto) {
+		super();
+		if (hora >= 0 && hora < 24) {
+			this.hora = hora;
+		}
+		if (minuto >= 0 && minuto < 60) {
+			this.minuto = minuto;
+		}
+	}
+
+	public void inc() {
+		if (minuto == 59) {
+			this.minuto = 0;
+
+			if (hora == 23) {
+				this.hora = 0;
+			}
+
+		} else {
+			this.minuto++;
+		}
+
+	}
+
+	public boolean setMinutos(int minutos) {
+		boolean valido = false;
+
+		if (minutos >= 0 && minutos < 60) {
+			this.minuto = minutos;
+			valido = true;
+		}
+
+		return valido;
+	}
+
+	public boolean setHora(int hora) {
+		boolean valido = false;
+
+		if (hora >= 0 && hora < 23) {
+			this.hora = hora;
+			valido = true;
+		}
+
+		return valido;
+	}
+
+	@Override
+	public String toString() {
+		String cadena = "";
+
+		if (hora < 10) {
+			cadena += 0;
+		}
+		
+		cadena += this.hora + ":";
+		
+		if (minuto < 10) {
+			cadena += 0;
+		}
+		
+		cadena += this.minuto;
+
+		return cadena;
+	}
+
 }
--- src/boletin1/ejercicio1/HoraExacta.java
+++ src/boletin1/ejercicio1/HoraExacta.java
@@ -1,5 +1,56 @@
 package boletin1.ejercicio1;
 
-public class HoraExacta {
+public class HoraExacta extends Hora{
+	
+	private double segundo = 0;
 
+	public HoraExacta(int hora, int minuto, double segundo) {
+		super(hora, minuto);
+		
+		if (segundo >= 0 && segundo < 60) {
+			this.segundo = segundo;
+		}
+		
+	}
+	
+	public boolean setSegundo (double segundo) {
+		boolean valido = false;
+		
+		if (segundo >= 0 && segundo < 60) {
+			valido = true;
+		}
+		
+		return valido;
+	}
+	
+	@Override
+	public void inc () {
+		
+		if (segundo == 59) {
+			this.segundo = 0;
+			
+			super.inc();
+			
+		} else {
+			this.segundo++;
+		}
+		
+	}
+	
+	@Override
+	public String toString () {
+		String cadena = "";
+		
+		super.toString();
+		
+		cadena += ":";
+		
+		if (segundo < 10) {
+			cadena += 0;
+		}
+		
+		cadena += this.segundo;
+		
+		return cadena;
+	}
 }
